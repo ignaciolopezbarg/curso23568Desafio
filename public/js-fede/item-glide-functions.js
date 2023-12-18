6c9ef9cae289ac42c00e18b0d62fd5c2b071dbbf
fedemelgin <federicomelgin2015@gmail.com>
2023-12-17 22:53:14 -0300

    hago dinamico el index y glide para productos aleatorios

--- public/js-fede/item-glide-functions.js
+++ public/js-fede/item-glide-functions.js
@@ -2,7 +2,7 @@
 // funcion que retorna 3 productos aleatorios
 function elegirProductosRandom(itemsFiltrados){
   
-  let productosFiltrados = itemsFiltrados.filter(item => !condicion(item)) // filtra todos los elementos que no sea el item seleccionado
+  let productosFiltrados = itemsFiltrados
   let productosAleatorios = [];
   
   while(productosAleatorios.length < 3){
@@ -16,76 +16,5 @@ function elegirProductosRandom(itemsFiltrados){
   }
   return productosAleatorios;
 }
-    
-function condicion(item){
-  return item.product_name === product.nombre;
-}
-    
-    // se crea funcion que mete codigo del glide en html dinamicamente con productos aleatorios
-function glideSelect(gliderContainer){
-  return (gliderContainer.innerHTML = elegirProductosRandom(productos).map((x) => {
-    return `
-      <li class="slider__item glide__slide">
-        <article class="card-item">
-        <!-- a este ultimo no seguimos con Bem, pues es una especie de tarjeta que se repite en las distintas paginas -->
-        <a class="card-item__link" href="./item.html">
-          <picture class="card-item__cover">
-          <span class="card-item__tag">NUEVO</span>
-          <img
-          class="card-item__img--front"
-          src="/public/img/multimedia/${x.product_image_front}"
-          alt="${x.product_description}"
-          />
-          <img
-          class="card-item__img--back"
-          src="/public/img/multimedia/${x.product_image_back}"
-          alt="${x.product_description}"
-          />
-          </picture>
-          <div class="card-item__content">
-            <p class="card-item__license">${x.categoria}</p>
-            <h4 class="card-item__name">${x.product_name}</h4>
-            <p class="card-item__price">$${x.product_price}</p>
-            <p class="card-item__promo">3 CUOTAS SIN INTERES</p>
-          </div>
-          </a>
-          </article>
-        </li>
-    `
-  }).join(""));
-}
-    const productoGlide =
-    {
-      nombre: "",
-      categoria: "", 
-      precio: "",
-      descripcion: "",
-      imgFront: "",
-      imgBack: ""
-    };
-document.addEventListener("DOMContentLoaded", function (){
-  const gliderContainer = document.querySelector(".slider__items");
-  glideSelect(gliderContainer);
-  const itemsGlide = document.querySelectorAll(".card-item__link");
-  const itemsGlideName = document.querySelectorAll(".card-item__name");
-  const itemsGlideCategoria = document.querySelectorAll(".card-item__license");
-  const itemsGlidePrecio = document.querySelectorAll(".card-item__price");
-  const itemsGlideImgFront = document.querySelectorAll(".card-item__img--front");
-  const itemsGlideImgBack = document.querySelectorAll(".card-item__img--back");
-  console.log(itemsGlideCategoria)
-  itemsGlideCategoria.forEach((item, index) => {
-    console.log(`Item ${index + 1}: ${item.textContent}`);
-  });
-  itemsGlide.forEach((itemsGlide,i) => {
-    itemsGlide.addEventListener('click', () => {
-      productoGlide["nombre"] = itemsGlideName[i].textContent;
-      productoGlide["categoria"] = itemsGlideCategoria[i].textContent;
-      productoGlide["precio"] = itemsGlidePrecio[i].textContent;
-      productoGlide["descripcion"] = itemsGlideImgFront[i].getAttribute("alt");
-      productoGlide["imgFront"] = itemsGlideImgFront[i].getAttribute("src");
-      productoGlide["imgBack"] = itemsGlideImgBack[i].getAttribute("src");
-      localStorage.setItem("selectProduct",JSON.stringify(productoGlide));
-    });
-  });
-});
 
+module.exports = elegirProductosRandom;
--- src/controllers/main.controller.js
+++ src/controllers/main.controller.js
@@ -1,21 +1,34 @@
-// const {getOne} = require ('../models/productModel');
-// const {getAll} = require ('../models/productModel');
+const path = require("path");
+const {getAll,getLicences} = require ('../models/product.model');
+const elegirProductosRandom = require('../../public/js-fede/item-glide-functions');
 module.exports ={
-    home:
-    (req,res)=>{
-        res.render('inicio',{
-            title:"Home"
-        })},
+    home: async(req,res)=>{
+        const originalLicences = await getLicences();
+        const licences = originalLicences.slice(0, 3);
+        const data = await getAll()
+        const itemsRandom = await elegirProductosRandom(data) // guardo 3 productos random sin repetir
+        
+        console.log(itemsRandom);
+        res.render(path.resolve(__dirname, "../views/inicio.ejs"),{
+            title:"Home",
+            licences,
+            itemsRandom
+        })
+    },
+
         //A la ruta le agregue un segundo parametro con el title del head, que esta en la carpeta partials header.ejs
-    contact:
-    (req,res)=>
-    res.render('Route for Contact View',{
-        title:'Contacto'
-    }),
-    about:
-    (req,res)=>
-    res.send('Route for About View'),
-    faqs:
-    (req,res)=>
-    res.send('Route for Faqs View')
+    contact: (req,res)=>{
+        res.render('Route for Contact View',{
+            title:'Contacto'
+        })
+    },
+
+    about: (req,res)=>{
+
+        res.send('Route for About View')
+    },
+
+    faqs:(req,res)=>{
+        res.send('Route for Faqs View')
+    },
 };
