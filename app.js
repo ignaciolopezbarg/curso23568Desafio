427090a5fa08e381309de6115891a69b3abb1320
ignacio lopez barg <ignaciolopezbar@hotmail.com>
2023-12-18 14:24:38 -0300

    cambio const port

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const shopRoutes = require("./src/routes/shop.routes.js");
 const adminRoutes = require("./src/routes/admin.routes.js");
 const authRoutes = require("./src/routes/auth.routes.js");
 
-const PORT = 4001 || 3306;
+const PORT = 4000 ;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./src/views"));
